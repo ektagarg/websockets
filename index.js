edfa6b895de4b7d6943f448c86c82202616ce20c
ektagarg <ekta@samayla.com>
2019-01-06 13:50:51 +0530

    Tweak: emitting and broadcasting messages

--- index.js
+++ index.js
@@ -15,5 +15,11 @@ var io = socket(server);
 io.on('connection', (socket) => {
 
     console.log('made socket connection', socket.id);
+    socket.on('chat',function(data){
+        io.emit('chat',data);
+    })
 
+    socket.on('typing', function(data){
+        socket.broadcast.emit('typing',data);
+    })
 });
